87a2ce1d4a8cd71e1122ccd93da0d1effed911a0
llq6 <984673705@qq.com>
2018-06-02 10:24:58 +0800

    banner

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: "./src/index.js",
   output: {
-      path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
   }
 };
