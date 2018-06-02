e89c1544da80b3aa8b01bfbc1caa64d5da3551d8
llq6 <984673705@qq.com>
2018-06-02 09:32:45 +0800

    banner

--- src/index.js
+++ src/index.js
@@ -1,22 +1,55 @@
 import { h, app } from "hyperapp"
 
 const state = {
-  index: 0
+  index: 0,
+  amount: 3,
+
 }
 
 const actions = {
-  next: value => state => ({ index: state.index - value }),
-  previous: value => state => ({ index: state.index + value })
+  next: function(value){
+    return(
+      function(state){
+        var newIndex;
+        if(newIndex=-state.amount){
+          newIndex=0
+        }else{
+          newIndex=state.index+1
+        }
+        var obj={
+          index:newIndex
+        }
+        return obj;
+      } 
+    )
+  },
+
+  previous:function(value){
+    return(
+      function(state){
+        var nexIndex;
+        if(newIndex=0){
+          newIndex=-state.amount
+        }else{
+          newIndex=state.index-1
+        }
+        var obj={
+          index:newIndex
+        }
+        return obj;
+      }
+    )
+  }
 }
 
 const view = (state, actions) => (
   <div className="container">
     <div className="container-border center">
         <div className="carousel" style={"transform:translateX(" + state.index*500 + "px)"}>
-      <img src="./images/photo1.png"/>
-			<img src="./images/photo2.png"/>
-			<img src="./images/photo3.png"/>
-			<img src="./images/photo4.png"/>
+          <img src="./images/photo1.png"/>
+			    <img src="./images/photo2.png"/>
+			    <img src="./images/photo3.png"/>
+			    <img src="./images/photo4.png"/>
         </div>
         <button className="arrow arrow-right" onclick={() => actions.next(1)}>右边</button>
         <button className="arrow arrow-right" onclick={() => actions.previous(1)}>左边</button>
