601912811ae1a910a686691cfad9a0a1ee2c90a7
llq6 <984673705@qq.com>
2018-06-27 21:41:40 +0800

    bannerr

--- src/index.js
+++ src/index.js
@@ -6,12 +6,11 @@ const state = {
 }
 
 const actions = {
-  next: value => state => ({ index: state.index - value }),
-  /*function(value){
+  next: function(value){
     return(
       function(state){
         var newIndex;
-        if(newIndex=-state.amount+1){
+        if(state.index>=state.amount-1){
           newIndex=0
         }else{
           newIndex=state.index+1
@@ -22,16 +21,14 @@ const actions = {
         return obj;
       } 
     )
-  },*/
+  },
 
-  previous:value => state => ({ index: state.index + value })
-}
-  /*function(value){
+  previous:function(value){
     return(
       function(state){
-        var nexIndex;
-        if(newIndex=0){
-          newIndex=-state.amount+1
+        var newIndex;
+        if(state.index<=0){
+          newIndex=state.amount-1
         }else{
           newIndex=state.index-1
         }
@@ -41,13 +38,24 @@ const actions = {
         return obj;
       }
     )
-  }
-}*/
+  },
+
+  /*second:function(value){
+    return(
+      function(state){
+        index:value-1
+      }
+    )
+  }*/
+}
 
 const view = (state, actions) => (
   <div className="container">
     <div className="container-border center">
-        <div className="carousel" style={"transform:translateX(" + state.index*500 + "px)"}>
+        <div className="carousel"
+         style={{
+          transform: "translateX(" + -1*state.index*500 + "px)"
+          }}>
           <img src="./images/photo1.png"/>
 			    <img src="./images/photo2.png"/>
 			    <img src="./images/photo3.png"/>
@@ -55,6 +63,12 @@ const view = (state, actions) => (
         </div>
         <button className="arrow right-arrow" onclick={() => actions.next(1)}>右边</button>
         <button className="arrow left-arrow" onclick={() => actions.previous(1)}>左边</button>
+        <div class="buttons">
+				  <span class="term onTerm">1</span>
+				  <span class="term  onclick={() => actions.second(2)}">2</span>
+				  <span class="term">3</span>
+				  <span class="term">4</span>
+			  </div>
     </div>
   </div>
 )
